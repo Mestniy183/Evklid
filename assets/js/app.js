66c068c6068ff651690970ced34d2f5f7f5a854c
Kirill <k187m18733@gmail.com>
2024-05-08 09:19:18 +0300

    fix: fix repo

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const navList = document.querySelector('.nav-list')
+const btnBurger = document.querySelector('.nav-burger')
+btnBurger.addEventListener('click', function () {
+    btnBurger.classList.toggle('active')
+    navList.classList.toggle('active')
+})
+var swiper = new Swiper(".swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
