0a6158078ab27664503b36e43fa4266edb62393b
Mestniy183 <ken-zero@ya.ru>
2024-06-06 08:48:13 +0300

    feat сделали 1024

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
-const navList = document.querySelector('.nav-list')
-const btnBurger = document.querySelector('.nav-burger')
+const navList = document.querySelector('.nav-list');
+const btnBurger = document.querySelector('.nav-burger');
 btnBurger.addEventListener('click', function () {
-    btnBurger.classList.toggle('active')
-    navList.classList.toggle('active')
-})
-var swiper = new Swiper(".swiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  btnBurger.classList.toggle('active');
+  navList.classList.toggle('active');
+});
+var swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  allowTouchMove: true,
 });
